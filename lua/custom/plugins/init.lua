fc8fa4de54aeb1c2fabd358f7543826f020b9511
Majcin9 <102909190+Majcin9@users.noreply.github.com>
2024-11-25 18:45:25 +0100

    cos

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,12 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
+
+require('lazy').setup {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon1',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+}
 -- See the kickstart.nvim README for more information
 return {}
